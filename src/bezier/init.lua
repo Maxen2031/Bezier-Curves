62fe8be8194835518e37774a6efd79c37689a4f4
= <ardayildirim05@icloud.com>
2022-08-24 13:28:59 +0200

    Version 0.1.2

--- src/bezier/init.lua
+++ src/bezier/init.lua
@@ -2,8 +2,15 @@
 
 local bezier = {}
 
-function bezier:CubicBezierCurver()
-    
+function bezier:CubicBezierCurve(camera, start, goal)
+    local p0 = camera.CFrame
+    local p1 = camera.CFrame * CFrame.new(10, 0, 0)
+    local p2 = goal.CFrame * CFrame.new(10, 0, 0)
+    local p3 = goal
+
+    for i = 1, 100 do
+        --local p1 = 
+    end
 end
 
 return bezier
