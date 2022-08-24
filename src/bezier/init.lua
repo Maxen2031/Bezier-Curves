f331048164306afc1f14eb874619e75839e6a9c5
Arda Yildirim <ardayildirim05@icloud.com>
2022-08-24 18:44:44 +0200

    Version: 0.2

--- src/bezier/init.lua
+++ src/bezier/init.lua
@@ -8,18 +8,15 @@ function bezier:CubicBezierCurve(camera, goal)
     local p2 = goal.CFrame * CFrame.new(10, 0, 0)
     local p3 = goal
 
-    local A = nil
-    local B = nil
-    local C = nil
-    local D = nil
-    local E = nil
-
-    local cframes = {
-        {value = nil; goal = p0; }
-    }
-
+    local A = p0
+    local B = p1
+    local C = p2
+    local D = A
+    local E = B
+    local P = D
+    
     for i = 1, 100 do
-        CFrame CFrame:lerp()
+        
     end
 end
 
